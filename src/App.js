38b9ff7c30de57128c2be10db8bdba0e6ac705d7
Atsuhiro Teshima <hiro@lingualbox.com>
2019-05-23 14:13:17 +0900

    Add React Helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Top from './Top';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>MyQuiz App</title>
+        <meta name="description" content="CodeGritのReactコースレッスン10のために作られたクイズアプリです。" />
+      </Helmet>
       <Top />
     </div>
   );
